fd26f14d817c07c96eb4b1774121d7bebe4b3842
GeorgiySergeev <126503204+GeorgiySergeev@users.noreply.github.com>
2023-07-21 21:30:43 +0300

    #1commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const iframe = document.getElementById('vimeo-player'); 
+const player = new Player(iframe);// создаем функцию прототипом которой есть ф-я Плеер
+
+player.on(
+  'timeupdate',
+  throttle(a => {
+    localStorage.setItem('videoplayer-current-time', a.seconds);
+  }, 1000)
+);
+
+let currentTime = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+//1.refs
+const formEl = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('input[name="email"]');
+const messageEl = document.querySelector('textarea[name="message"]');
+
+
+formEl.addEventListener('input', throttle(onInput, 500));
+
+function onInput() {
+  const storageObject = { email: inputEl.value, message: messageEl.value };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(storageObject));
+}
+
+formEl.addEventListener('submit', evt => {
+  evt.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.clear();
+  evt.target.reset();
+});
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+const storageData = load('feedback-form-state');
+if (storageData) {
+  inputEl.value = storageData.email;
+  messageEl.value = storageData.message;
+}
